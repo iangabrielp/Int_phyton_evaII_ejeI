701d521cf7bfec972bff9fef585c8de62f7d2ac5
Your Name <you@example.com>
2025-07-11 20:42:08 -0500

    final commit

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ from selenium.webdriver.common.keys import Keys
 import time
 from credenciales import USUARIO, CONTRASENIA
 
-# Paso 1: Abrir página
+# Abrir página
 url = "https://www.demoblaze.com/index.html"
 driver = webdriver.Chrome()
 driver.get(url)
 time.sleep(3)
 
-# Paso 2: Registro de usuario
-registro = driver.find_element(By.ID, "signin2")
+# Registro de usuario
+registro = driver.find_element(By.ID, '//*[@id="signin2"]')
 registro.click()
 time.sleep(3)
 
-usuario_input = driver.find_element(By.ID, "sign-username")
+usuario_input = driver.find_element(By.ID, '//*[@id="sign-username"]')
 usuario_input.send_keys(USUARIO)
 
-contrasenia_input = driver.find_element(By.ID, "sign-password")
+contrasenia_input = driver.find_element(By.ID, '//*[@id="sign-password"]')
 contrasenia_input.send_keys(CONTRASENIA)
 
-boton_registro = driver.find_element(By.XPATH, '//button[text()="Sign up"]')
+boton_registro = driver.find_element(By.XPATH, '//*[@id="signInModal"]/div/div/div[3]/button[2]')
 boton_registro.click()
 time.sleep(2)
 
@@ -35,28 +35,28 @@ except:
 
 time.sleep(3)
 
-# Paso 3: Login
-login = driver.find_element(By.ID, "login2")
+#  Login
+login = driver.find_element(By.ID, '//*[@id="login2"]')
 login.click()
 time.sleep(3)
 
-login_usuario = driver.find_element(By.ID, "loginusername")
+login_usuario = driver.find_element(By.ID, '//*[@id="loginusername"]')
 login_usuario.send_keys(USUARIO)
 
-login_contra = driver.find_element(By.ID, "loginpassword")
+login_contra = driver.find_element(By.ID, '//*[@id="loginpassword"]')
 login_contra.send_keys(CONTRASENIA)
 
-login_boton = driver.find_element(By.XPATH, '//button[text()="Log in"]')
+login_boton = driver.find_element(By.XPATH, '//*[@id="logInModal"]/div/div/div[3]/button[2]')
 login_boton.click()
 time.sleep(4)
 
-# Paso 4: Seleccionar dos productos
+#  Seleccionar dos productos
 def agregar_producto(nombre_producto):
-    link_producto = driver.find_element(By.LINK_TEXT, nombre_producto)
+    link_producto = driver.find_element(By.LINK_TEXT, '//*[@id="tbodyid"]/div[1]/div/div/h4/a')
     link_producto.click()
     time.sleep(2)
 
-    boton_agregar = driver.find_element(By.LINK_TEXT, "Add to cart")
+    boton_agregar = driver.find_element(By.LINK_TEXT, '//*[@id="tbodyid"]/div[2]/div/a')
     boton_agregar.click()
     time.sleep(2)
 
@@ -72,12 +72,12 @@ def agregar_producto(nombre_producto):
 agregar_producto("Samsung galaxy s6")
 agregar_producto("Nexus 6")
 
-# Paso 5: Ir al carrito y realizar pedido
-carrito = driver.find_element(By.ID, "cartur")
+# Ir al carrito y realizar pedido
+carrito = driver.find_element(By.ID, '//*[@id="cartur"]/font/font')
 carrito.click()
 time.sleep(3)
 
-place_order = driver.find_element(By.XPATH, '//button[text()="Place Order"]')
+place_order = driver.find_element(By.XPATH, '//*[@id="page-wrapper"]/div/div[2]/button')
 place_order.click()
 time.sleep(2)
 
@@ -89,15 +89,15 @@ driver.find_element(By.ID, "card").send_keys("1234567890123456")
 driver.find_element(By.ID, "month").send_keys("07")
 driver.find_element(By.ID, "year").send_keys("2025")
 
-driver.find_element(By.XPATH, '//button[text()="Purchase"]').click()
+driver.find_element(By.XPATH, '//*[@id="orderModal"]/div/div/div[3]/button[2]').click()
 time.sleep(3)
 
 # Confirmar
-driver.find_element(By.XPATH, '//button[text()="OK"]').click()
+driver.find_element(By.XPATH, '/html/body/div[11]/div[7]/div/button').click()
 time.sleep(2)
 
-# Paso 6: Logout
-logout = driver.find_element(By.ID, "logout2")
+#  Logout
+logout = driver.find_element(By.ID, '//*[@id="logout2"]')
 logout.click()
 
 # Fin
